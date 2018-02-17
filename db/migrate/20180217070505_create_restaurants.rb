33abe022230024f7028bfb8eb7a0eefe7903fac3
Benjamin Skiff <benskiff30@gmail.com>
2018-02-17 15:06:23 +0800

    rails new

---
+++ db/migrate/20180217070505_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :category
+      t.string :phone
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
